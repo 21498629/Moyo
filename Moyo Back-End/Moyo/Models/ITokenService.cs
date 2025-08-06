d22d7b407f3f89fe46252b2cbcd8cae1591cee51
Refilwe Moeketsane <102926514+21498629@users.noreply.github.com>
2025-08-06 08:20:44 +0200

    Delete Moyo Back-End directory

--- Moyo Back-End/Moyo/Models/ITokenService.cs
+++
@@ -1,9 +0,0 @@
-﻿using Moyo.View_Models;
-
-namespace Moyo.Models
-{
-    public interface ITokenService
-    {
-        string CreateToken(User user, IEnumerable<string> roles, IEnumerable<string> scopes);
-    }
-}
